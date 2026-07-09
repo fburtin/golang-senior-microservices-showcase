71478f0abad6755f60409e48d484c3e94c367fb3
Francisco Burtin <francisco.burtin@cscgfm.com>
2026-07-09 10:59:24 -0300

    Refactor application bootstrap

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,53 +1,8 @@
 package main
 
-import (
-	"context"
-	"net/http"
-	"os"
-
-	"github.com/fburtin/golang-senior-microservices-showcase/internal/app"
-	"github.com/fburtin/golang-senior-microservices-showcase/internal/config"
-	"github.com/fburtin/golang-senior-microservices-showcase/internal/handlers"
-	"github.com/fburtin/golang-senior-microservices-showcase/internal/logger"
-	"github.com/fburtin/golang-senior-microservices-showcase/internal/repositories"
-	"github.com/fburtin/golang-senior-microservices-showcase/internal/services"
-
-	"go.mongodb.org/mongo-driver/v2/mongo"
-	"go.mongodb.org/mongo-driver/v2/mongo/options"
-)
+import "github.com/fburtin/golang-senior-microservices-showcase/internal/app"
 
 func main() {
-	cfg := config.Load()
-	appLogger := logger.New()
-
-	ctx, cancel := context.WithTimeout(context.Background(), cfg.MongoTimeout)
-	defer cancel()
-
-	mongoClient, err := mongo.Connect(options.Client().ApplyURI(cfg.MongoURI))
-	if err != nil {
-		appLogger.Error("failed to connect to MongoDB", "error", err)
-		os.Exit(1)
-	}
-
-	err = mongoClient.Ping(ctx, nil)
-	if err != nil {
-		appLogger.Error("failed to ping MongoDB", "error", err)
-		os.Exit(1)
-	}
-
-	database := mongoClient.Database(cfg.MongoDatabase)
-
-	customerRepository := repositories.NewMongoCustomerRepository(database)
-	customerService := services.NewCustomerService(customerRepository)
-	customerHandler := handlers.NewCustomerHandler(customerService)
-
-	router := app.NewRouter(customerHandler, appLogger)
-
-	appLogger.Info("HTTP server started", "port", cfg.Port)
-
-	err = http.ListenAndServe(":"+cfg.Port, router)
-	if err != nil {
-		appLogger.Error("HTTP server stopped", "error", err)
-		os.Exit(1)
-	}
+	application := app.New()
+	application.Run()
 }
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,1 +1,71 @@
 package app
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/fburtin/golang-senior-microservices-showcase/internal/config"
+	"github.com/fburtin/golang-senior-microservices-showcase/internal/handlers"
+	"github.com/fburtin/golang-senior-microservices-showcase/internal/logger"
+	"github.com/fburtin/golang-senior-microservices-showcase/internal/repositories"
+	"github.com/fburtin/golang-senior-microservices-showcase/internal/services"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+type App struct {
+	config config.Config
+	logger *slog.Logger
+	router http.Handler
+}
+
+func New() *App {
+	cfg := config.Load()
+	appLogger := logger.New()
+
+	database := connectMongo(cfg, appLogger)
+
+	customerRepository := repositories.NewMongoCustomerRepository(database)
+	customerService := services.NewCustomerService(customerRepository)
+	customerHandler := handlers.NewCustomerHandler(customerService)
+
+	router := NewRouter(customerHandler, appLogger)
+
+	return &App{
+		config: cfg,
+		logger: appLogger,
+		router: router,
+	}
+}
+
+func (a *App) Run() {
+	a.logger.Info("HTTP server started", "port", a.config.Port)
+
+	err := http.ListenAndServe(":"+a.config.Port, a.router)
+	if err != nil {
+		a.logger.Error("HTTP server stopped", "error", err)
+		os.Exit(1)
+	}
+}
+
+func connectMongo(cfg config.Config, appLogger *slog.Logger) *mongo.Database {
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.MongoTimeout)
+	defer cancel()
+
+	mongoClient, err := mongo.Connect(options.Client().ApplyURI(cfg.MongoURI))
+	if err != nil {
+		appLogger.Error("failed to connect to MongoDB", "error", err)
+		os.Exit(1)
+	}
+
+	err = mongoClient.Ping(ctx, nil)
+	if err != nil {
+		appLogger.Error("failed to ping MongoDB", "error", err)
+		os.Exit(1)
+	}
+
+	return mongoClient.Database(cfg.MongoDatabase)
+}
