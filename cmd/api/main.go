d326bb3d65da3265e49fc6f1ae5bbe6531e2ff28
Francisco Burtin <francisco.burtin@cscgfm.com>
2026-07-09 10:20:04 -0300

    Add structured logging

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"log"
 	"net/http"
+	"os"
 
 	"github.com/fburtin/golang-senior-microservices-showcase/internal/config"
 	"github.com/fburtin/golang-senior-microservices-showcase/internal/handlers"
+	"github.com/fburtin/golang-senior-microservices-showcase/internal/logger"
 	"github.com/fburtin/golang-senior-microservices-showcase/internal/repositories"
 	"github.com/fburtin/golang-senior-microservices-showcase/internal/services"
+
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
@@ -17,24 +19,30 @@ import (
 func main() {
 
 	cfg := config.Load()
+	appLogger := logger.New()
 
 	ctx, cancel := context.WithTimeout(context.Background(), cfg.MongoTimeout)
 	defer cancel()
-
 	mongoClient, err := mongo.Connect(options.Client().ApplyURI(cfg.MongoURI))
 	if err != nil {
-		log.Fatal(err)
+		appLogger.Error(
+			"failed to connect to MongoDB",
+			"error", err,
+		)
+		os.Exit(1)
 	}
-
 	err = mongoClient.Ping(ctx, nil)
 	if err != nil {
-		log.Fatal(err)
+		appLogger.Error(
+			"failed to ping MongoDB",
+			"error", err,
+		)
+		os.Exit(1)
 	}
 
 	database := mongoClient.Database(cfg.MongoDatabase)
 
 	customerRepository := repositories.NewMongoCustomerRepository(database)
-
 	customerService := services.NewCustomerService(customerRepository)
 	customerHandler := handlers.NewCustomerHandler(customerService)
 
@@ -42,8 +50,19 @@ func main() {
 	http.HandleFunc("/customers", customerHandler.HandleCustomers)
 	http.HandleFunc("/customers/{id}", customerHandler.HandleCustomerByID)
 
-	log.Println("Server started at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	appLogger.Info(
+		"HTTP server started",
+		"port", cfg.Port,
+	)
+
+	err = http.ListenAndServe(":"+cfg.Port, nil)
+	if err != nil {
+		appLogger.Error(
+			"HTTP server stopped",
+			"error", err,
+		)
+		os.Exit(1)
+	}
 }
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
